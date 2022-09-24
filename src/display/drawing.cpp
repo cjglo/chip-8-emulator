df62ee3f1e519fb4eecaf584cffa3623914dbdec
cjglo <chris.j.gallo3@gmail.com>
2022-09-24 15:15:16 -0400

    additional test

--- src/display/drawing.cpp
+++ src/display/drawing.cpp
@@ -14,7 +14,7 @@ void updateDisplay(bool display[DISPLAY_HEIGHT][DISPLAY_WIDTH]) {
 		for(int j = 0; j<DISPLAY_WIDTH; j++) {
 
 			if(display[i][j]) {
-				DrawRectangle(WINDOW_LENGTH/2, WINDOW_HEIGHT/2, 30, 30, MAROON);
+				DrawRectangle(i, j, 10, 10, WHITE);
 			}
 
 		}
