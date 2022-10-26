d0264884d50a34b4faf95da8dd3058a35f20e102
ripley <ripley@xenomorph.alien>
2022-10-25 22:48:59 -0400

    quick main.g reorg

--- include/main.h
+++ include/main.h
@@ -10,11 +10,10 @@
 #include "fetch.h"
 #include "decode.h"
 
-void delayTimerCycle(uint8_t* timer, bool* onSwitch);
-void soundTimerCycle(uint8_t* timer, bool* onSwitch);
-
 // functions in main
 void initializeFontInMemory(uint8_t memory[MEMORY_SIZE], uint8_t fontData[FONT_DATA_SIZE]);
 void initializeMemory(uint8_t memory[MEMORY_SIZE]);
+void delayTimerCycle(uint8_t* timer, bool* onSwitch);
+void soundTimerCycle(uint8_t* timer, bool* onSwitch);
 
 #endif
