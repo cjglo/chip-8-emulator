7e3d2964a9eb9c6434b0ca35dfd726b72a6cedb0
ripley <ripley@xenomorph.alien>
2022-11-08 22:57:51 -0500

    added SetIndexRegister Command and reformatted files

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
 #include "../include/main.h"
 
-int main() {
+int main()
+{
     // Initialize all virtual hardware
     uint8_t Memory[MEMORY_SIZE]; // Memory
     initializeMemory(Memory);
-    bool Display[DISPLAY_HEIGHT][DISPLAY_WIDTH]; // Display
+    bool Display[DISPLAY_HEIGHT][DISPLAY_WIDTH];  // Display
     uint8_t fontData[FONT_DATA_SIZE] = FONT_DATA; // Font
     initializeFontInMemory(Memory, fontData);
     uint16_t Stack[STACK_SIZE]; // Stack
-    uint8_t DelayTimer = 255; // Delay Timer
-    uint8_t SoundTimer = 255; // Sound Timer
+    uint8_t DelayTimer = 255;   // Delay Timer
+    uint8_t SoundTimer = 255;   // Sound Timer
     bool delayTimerOnSwitch = true;
     bool soundTimerOnSwitch = true;
     uint12_struct PC; // Program Counter
     PC.bits = STARTING_REGISTER;
-    uint16_t IR = 0; // Index Register
+    uint16_t IR = 0;                               // Index Register
     uint8_t varRegisters[VARIABLE_REGISTERS_SIZE]; // Variable Registers
 
     // Starting timers, they execute independent of exec cycle
@@ -22,23 +23,24 @@ int main() {
     std::thread soundTimerRoutine(soundTimerCycle, &SoundTimer, &soundTimerOnSwitch);
 
     createWindow();
-    
-    while(!WindowShouldClose()) { // WindowShouldClose is raylib func that returns true on ESC or close button
-        // execution loop:
-            // fetch
-            uint16_t instruction = fetch(PC, Memory);
-            
-            // decode
-            std::unique_ptr<Command> command = decode(instruction);
 
-            // execute
-            command->execute(Memory, Display, Stack, varRegisters, PC, IR);
-	
-	    // in the future execution will updateDisplay and or close window
-	    // for testing I will leave updateDisplay in loop seperate
-	    // until ready to build out those instructions
-	    updateDisplay(Display);
-	    sleep(CLOCK_EXECUTION_DELAY); 
+    while (!WindowShouldClose())
+    {   // WindowShouldClose is raylib func that returns true on ESC or close button
+        // execution loop:
+        // fetch
+        uint16_t instruction = fetch(PC, Memory);
+
+        // decode
+        std::unique_ptr<Command> command = decode(instruction);
+
+        // execute
+        command->execute(Memory, Display, Stack, varRegisters, &PC, &IR);
+
+        // in the future execution will updateDisplay and or close window
+        // for testing I will leave updateDisplay in loop seperate
+        // until ready to build out those instructions
+        updateDisplay(Display);
+        sleep(CLOCK_EXECUTION_DELAY);
     }
     // allow timers to finish so not abrupt abort
     delayTimerOnSwitch = false;
@@ -50,42 +52,58 @@ int main() {
     return 0;
 }
 
-auto initializeFontInMemory(uint8_t memory[MEMORY_SIZE], uint8_t fontData[FONT_DATA_SIZE]) -> void {
-    for(int i = 0; i<FONT_DATA_SIZE; i++) {
-        memory[i+FONT_DATA_START] = fontData[i];
+auto initializeFontInMemory(uint8_t memory[MEMORY_SIZE], uint8_t fontData[FONT_DATA_SIZE]) -> void
+{
+    for (int i = 0; i < FONT_DATA_SIZE; i++)
+    {
+        memory[i + FONT_DATA_START] = fontData[i];
     }
 }
 
-auto initializeMemory(uint8_t memory[MEMORY_SIZE]) -> void {
+auto initializeMemory(uint8_t memory[MEMORY_SIZE]) -> void
+{
     // TODO: Load program here I believe
     // FOR TESTING
-    for(int i = 0; i<MEMORY_SIZE; i++) {
-        memory[i] = (i % 2 == 0) ? 0x6F : 0x12;
+    for (int i = 0; i < MEMORY_SIZE; i++)
+    {
+        memory[i] = (i % 2 == 0) ? 0x7F : 0x12;
     }
 }
 
 // NOTE: Timers are currently dependent on WindowShouldClose, which is external.  Maybe should pass as parameter?
-auto delayTimerCycle(uint8_t* timer, bool* onSwitch) -> void {
-    while(true) {
+auto delayTimerCycle(uint8_t *timer, bool *onSwitch) -> void
+{
+    while (true)
+    {
         sleep(TIMER_DELAY);
-        if(*timer == 0) {
+        if (*timer == 0)
+        {
             *timer = 255;
-        } else {
+        }
+        else
+        {
             *timer -= 1;
         }
-        if(!*onSwitch) break;
+        if (!*onSwitch)
+            break;
     }
 }
 
-auto soundTimerCycle(uint8_t* timer, bool* onSwitch) -> void {
-    while(true) {
+auto soundTimerCycle(uint8_t *timer, bool *onSwitch) -> void
+{
+    while (true)
+    {
         sleep(TIMER_DELAY);
-        if(*timer == 0) {
+        if (*timer == 0)
+        {
             *timer = 255;
-        } else {
+        }
+        else
+        {
             // TODO: Play Sound
             *timer -= 1;
         }
-        if(!*onSwitch) break;
+        if (!*onSwitch)
+            break;
     }
 }
